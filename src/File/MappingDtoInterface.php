77a6d21ac061740b469558b13ad010be012fcf0e
dvorski <dadeky@gmail.com>
2017-11-15 11:47:32 +0100

    Explanation

--- src/File/MappingDtoInterface.php
+++ src/File/MappingDtoInterface.php
@@ -4,7 +4,9 @@ namespace File;
 interface MappingDtoInterface
 {
     /**
-     * Gets the setter name to be called in order to set the value
+     * Gets the setter name to be called in order to set the value.
+     * For example if the $index argument is 3 the setter name which
+     * corresponds to index 3 is returned and called in the ParserService to set the value in the DTO
      * @param integer $index an index of a setter
      * @return array
      */
