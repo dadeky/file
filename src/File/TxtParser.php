83128fa705c1370b81faabfe3f9fb431a27fd200
ngaspari <33628128+ngaspari@users.noreply.github.com>
2020-04-29 14:46:28 +0200

    TxtParser

---
+++ src/File/TxtParser.php
@@ -0,0 +1,35 @@
+<?php
+namespace File;
+
+class TxtParser
+{
+    const DEFAULT_DELIMITER = "\t";
+    
+    public function parse(
+        array $lines,
+        MappingDtoInterface $mappingDto,
+        $delimiter = self::DEFAULT_DELIMITER
+    ){
+        $objects = [];
+        foreach ($lines as $line){
+            if (!empty($line))
+            {
+                $lineDataArray = explode($delimiter, rtrim($line));
+                if(count($lineDataArray) > 0){
+                    $object = clone $mappingDto;
+                    foreach ($lineDataArray as $key => $value)
+                    {
+                        $methodName = $mappingDto->getMappingSetter($key);
+                        if (method_exists($object, $methodName))
+                        {
+                            $object->{ $methodName }($value);
+                        }
+                    }
+                    $objects[] = $object;
+                }
+            }
+        }
+        return $objects;
+    }
+}
+
